d192384eb67df516ea349ce28a9098b61130bcf0
Lina KHALIFA <khalifa@mesu0.ib0.xa.dsi.upmc.fr>
2019-04-03 11:16:06 +0200

    updated starting examples

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-sudo add-apt-repository main
-sudo add-apt-repository universe
-sudo apt-get update
+#sudo add-apt-repository main
+#sudo add-apt-repository universe
+#sudo apt-get update
 
-sudo apt-get install gcc make cmake
-sudo apt-get install libpthread-stubs0-dev libgomp1 freeglut3-dev libxmu-dev libxi-dev libptscotch-dev liblapacke-dev
+#sudo apt-get install gcc make cmake
+#sudo apt-get install libpthread-stubs0-dev libgomp1 freeglut3-dev libxmu-dev libxi-dev libptscotch-dev liblapacke-dev
 
 cd ./sources/commons
 mkdir build
@@ -12,53 +12,56 @@ cmake ..
 make
 make install
 
-cd ./../../advection
+cd ../../advection
 mkdir build
 cd build
 cmake ..
 make
 make install
 
-cd ./../../elasticity
+cd ../../elasticity
 mkdir build
 cd build
 cmake ..
 make
 make install
 
-cd ./../../mshdistance
+cd ../../mshdistance
 mkdir build
 cd build
 cmake ..
 make
 make install
 
-cd ./../../medit
+cd ../../medit
 mkdir build
 cd build
 cmake ..
 make
 make install
 
-cd ./../../mmg3d
+cd ../../mmg3d
 mkdir build
 cd build
 cmake ..
 make
 make install
 
-cd ./../../mpd
+cd ../../mpd
 mkdir build
 cd build
 cmake ..
 make
 make install
 
-cd ./../../../
+cd ../../..
 mkdir outputFiles
 cd ./outputFiles
-mkdir exampleTetrahedra
-cp ./../inputFiles/exHfTet.info ./exampleTetrahedra/
-mkdir exampleHexahedra
-cp ./../inputFiles/exHfHex.info ./exampleHexahedra/
+mkdir quickstartTests
+cd ./quickstartTests
+mkdir test0
+cp ../../inputFiles/quickstart0.info ./test0
+mkdir test1
+cp ../../inputFiles/quickstart1.info ./test1
+cd ../..
 
